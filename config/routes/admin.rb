c1cc7023c379e0eb6736cadf8171e59862abce39
nejdetkadir <nejdetkadir.550@gmail.com>
2021-05-26 03:19:40 +0300

    Create new post modal

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -2,5 +2,7 @@ authenticate :user do
   scope '/', module: 'admin', as: 'admin' do
     get 'feed', to: 'pages#feed', as: 'feed'
     get 'profile', to: 'pages#profile', as: 'profile'
+
+    resources :posts
   end
 end
